a605e35bced63f695468181d1e218935052d4396
beginor <beginor@hotmail.com.com>
2015-03-25 06:04:05 +0800

    move to api proj to src, add client bower components.

---
+++ src/AspNetIdentity.WebApi/Controllers/AccountsController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using AspNetIdentity.WebApi.Data;
+using AspNetIdentity.WebApi.Models;
+using Microsoft.AspNet.Identity;
+using System.Net.Http;
+
+namespace AspNetIdentity.WebApi.Controllers {
+
+    [RoutePrefix("api/accounts")]
+    public class AccountsController : BaseApiController {
+
+        [Route("users")]
+        public IHttpActionResult GetUsers() {
+            return Ok(AppUserManager.Users.ToList().Select(u => TheModelFactory.Create(u)));
+        }
+
+        [Route("user/{id:guid}", Name = "GetUserById")]
+        public async Task<IHttpActionResult> GetUser(string id) {
+            var user = await AppUserManager.FindByIdAsync(id);
+
+            if (user != null) {
+                return Ok(TheModelFactory.Create(user));
+            }
+
+            return NotFound();
+
+        }
+
+        [Route("user/{username}")]
+        public async Task<IHttpActionResult> GetUserByName(string username) {
+            var user = await AppUserManager.FindByNameAsync(username);
+
+            if (user != null) {
+                return Ok(TheModelFactory.Create(user));
+            }
+
+            return NotFound();
+
+        }
+
+        public IHttpActionResult Login(LoginModel model) {
+            if (!ModelState.IsValid) {
+                return BadRequest(ModelState);
+            }
+            var user = AppUserManager.FindByName(model.Username);
+            if (user == null) {
+                return BadRequest(string.Format("User {0} does not exist.", model.Username));
+            }
+            if (!AppUserManager.CheckPassword(user, model.Password)) {
+                return BadRequest("Invalid password.");
+            }
+            var identity = AppUserManager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+            var auth = Request.GetOwinContext().Authentication;
+            auth.SignIn(identity);
+            return Ok();
+        }
+
+        [Route("create")]
+        public async Task<IHttpActionResult> CreateUser(CreateUserModel createUserModel) {
+            if (!ModelState.IsValid) {
+                return BadRequest(ModelState);
+            }
+
+            var user = new ApplicationUser {
+                UserName = createUserModel.Username,
+                Email = createUserModel.Email,
+                FirstName = createUserModel.FirstName,
+                LastName = createUserModel.LastName,
+                Level = 3,
+                JoinDate = DateTime.Now.Date
+            };
+
+            IdentityResult addUserResult = await AppUserManager.CreateAsync(user, createUserModel.Password);
+
+            if (!addUserResult.Succeeded) {
+                return GetErrorResult(addUserResult);
+            }
+
+            Uri locationHeader = new Uri(Url.Link("GetUserById", new { id = user.Id }));
+
+            return Created(locationHeader, TheModelFactory.Create(user));
+        }
+    }
+}
---
+++ src/AspNetIdentity.WebApi/app.js
@@ -0,0 +1,24 @@
+﻿/// <reference path="D:\Projects\asp-net-identity\src\AspNetIdentity.WebApi\bower_components/angular/angular.js" />
+/// <reference path="D:\Projects\asp-net-identity\src\AspNetIdentity.WebApi\bower_components/angular-route/angular-route.js" />
+
+angular.module('demoApp', ['ngRoute'])
+.controller('Mai nController', function ($scope) {
+})
+
+.controller('LoginController', function ($scope) {
+
+})
+
+.controller('RegisterController', function ($scope) {
+
+})
+
+.config(function ($routeProvider, $locationProvider) {
+    $routeProvider.when('/login', {
+        templateUrl: 'login.html',
+        controller: 'LoginController'
+    })
+    .when('/register', {
+        templateUrl:
+    })
+});
